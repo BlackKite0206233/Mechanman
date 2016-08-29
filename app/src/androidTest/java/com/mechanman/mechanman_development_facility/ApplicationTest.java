5d9270bd30c999aa15def7b1151fe673d27a2a84
blackkite0206233 <samuel991640@gmail.com>
2016-08-29 18:06:40 +0800

    init

---
+++ app/src/androidTest/java/com/mechanman/mechanman_development_facility/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.mechanman.mechanman_development_facility;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
