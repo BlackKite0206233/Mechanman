ff196f8ac60c385270997e59da10556f789468fb
BlackKite0206233 <samuel991640@gmail.com>
2016-08-30 11:05:57 +0800

    start page setting

--- app/src/main/java/com/mechanman/mechanman_development_facility/StartActivity.java
+++ app/src/main/java/com/mechanman/mechanman_development_facility/StartActivity.java
@@ -2,12 +2,34 @@ package com.mechanman.mechanman_development_facility;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class StartActivity extends AppCompatActivity {
+
+    private Button linkButton;
+    private Button developHistory;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        linkButton = (Button) findViewById(R.id.linkButton);
+        developHistory = (Button) findViewById(R.id.developHistory);
+
+        linkButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        developHistory.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
